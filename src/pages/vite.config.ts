74e8162d71402a634047b274947718c9a5720b37
cleilsonnog <cleilsonnogueira45@gmail.com>
2025-10-26 16:39:27 -0300

    Inserido PWA

---
+++ src/pages/vite.config.ts
@@ -0,0 +1,49 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Calculadora de Trade Binário",
+        short_name: "Calculadora Trade",
+        description:
+          "Gerencie suas operações com precisão, controle de risco e cálculos automáticos de Martingale.",
+        theme_color: "#18181b", // Cor de fundo do seu app (background color from tailwind.config.js)
+        background_color: "#18181b",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
